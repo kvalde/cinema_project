8b3869692aaa563916dc57ad46823f42bd56dd83
kvalde <kapeliushnyivolodymyr@gmail.com>
2020-07-06 18:02:08 +0300

    sample

--- js/index.js
+++ js/index.js
@@ -71,5 +71,5 @@ container.addEventListener('click', e => {
 updateSelectedCount();
 
 buyTicketsButton.addEventListener('click', e => {
-  document.querySelectorAll('selected').forEach(element => element.classList.add('occupied'));
+  document.querySelectorAll('.selected').forEach(element => element.classList.add('occupied'));
 })
